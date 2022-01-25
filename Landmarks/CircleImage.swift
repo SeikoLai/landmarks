7ab06a973d9b6687e514ea8f12ba619c8e24592e
sam.lai <sam.lai@udnshopping.com>
2022-01-25 23:30:50 +0800

    chore: completed Creating and Combining Views Section 5 Use SwiftUI Views From Other Frameworks step 2.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sam Lai on 2022/1/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Sam Lai on 2022/1/25.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
