6f91a8a1cf18302ae277463fd22a405718fc9690
sam.lai <sam.lai@udnshopping.com>
2022-02-01 11:47:58 +0800

    feat: add map view.

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    /**
+     You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
+     */
+    @State private var region = MKCoordinateRegion(
+    center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+    span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        /**
+         By prefixing a state variable with $, you pass a binding, which is like a reference to the underlying value. When the user interacts with the map, the map updates the region value to match the part of the map that’s currently visible in the user interface.
+         */
+        Map(coordinateRegion: $region)
     }
 }
 
